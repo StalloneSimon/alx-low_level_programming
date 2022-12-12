5036f0c56bda6e01dd84000789532d7a1298ea61
StalloneSimon <stallonendungu@gmail.com>
2022-12-12 22:37:52 +0000

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,17 @@
 int main(void)
 {
 	char uppercase;
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
 
 	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
 		putchar(uppercase);
-
+	}
 	putchar('\n');
 
 	return (0);
