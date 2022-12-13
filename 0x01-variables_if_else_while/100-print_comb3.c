8026eaa7e9f4b871a741ac4302257b3271517bd7
StalloneSimon <stallonendungu@gmail.com>
2022-12-13 10:48:46 +0000

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,21 +12,24 @@ int main(void)
 	int no1;
 	int no2;
 
-	for (no1 = 0; no1 < 10; no1++)
+	for (no1 = 0; no1 < 9; no1++)
 	{
-		for (no2 = 0; no2 < 10; no2++)
-		{
-			if (!((no1 == no2) || (no2 > no1)))
+		no2 = no1 + 1;
+			
+		do{
 
-			putchar((no1 % 10) + '0');
-			putchar((no2 % 10) + '0');
+			putchar(no1 + '0');
+			putchar(no2 + '0');
 
-			if (no1 == 9 && no2 == 8)
-				continue;
+			if (no2 < 8)
+			{
 
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(32);
+			}
+			no2++;
 		}
+		while (no2 < 10);
 	}
 	putchar('\n');
 
