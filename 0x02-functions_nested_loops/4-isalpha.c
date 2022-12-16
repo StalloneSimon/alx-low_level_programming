a10ace3a75718a804a0bc77b8d0d6a9fd40e59a0
StalloneSimon <stallonendungu@gmail.com>
2022-12-16 22:52:01 +0000

    dealing with numbers

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks whether parameter is char and
+ *	if lowercase or uppercase
+ *@c: the char were checking
+ * Return: 1 if lowercase or uppercase else return 0
+ */
+int _isalpha(int c)
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ * @n: the number we are checking
+ *
+ * Return: the absolute number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
