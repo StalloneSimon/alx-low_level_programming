b25f0bdb4adf16314a84bbeb9d33733d5173bdf2
StalloneSimon <stallonendungu@gmail.com>
2022-12-16 19:58:52 +0000

    trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <unistd.h>
-#include <math.h>
+#include "main.h"
 /**
  * main - prints _putchar
  *
- * Return: Always 0.
+ * Return: 0
  */
-int _puthchar(char c)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	return (write(1, &c, 1));
-
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -15,4 +17,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif /*MAIN_H*/
