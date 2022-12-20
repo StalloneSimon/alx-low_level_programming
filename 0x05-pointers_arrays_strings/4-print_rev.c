ab137a3618ab02004445b24a44bab71c4ab2c062
StalloneSimon <stallonendungu@gmail.com>
2022-12-20 03:15:42 +0000

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,17 @@
  */
 void print_rev(char *s)
 {
-	int length = 0, i;
+	int i;
 
-	while (s[i++])
-		length++;
-
-	for (i = length - 1; i >= 0; i--)
-		_putchar(s[i]);
-
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	
+       while(s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
 	_putchar('\n');
 }
