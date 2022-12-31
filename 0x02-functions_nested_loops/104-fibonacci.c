688c3dfe00e140577781aff0e4aba309f8a8dcc9
StalloneSimon <stallonendungu@gmail.com>
2022-12-31 12:42:20 +0000

    trickiest so far

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,15 +5,15 @@ void main()
 	unsigned long int fib1 = 0, fib2 = 1, fib3, num, count = 0;
 
 	printf("Enter the value of num \n");
-	scanf("%ld", &num);
-	printf("First %ld FIBONACCI numbers are ...\n", num);
-	printf("%ld\n", fib1);
+	scanf("%lu", &num);
+	printf("First %lu FIBONACCI numbers are ...\n", num);
+	printf("%lu\n", fib1);
 	count = 2;
 	while (count < num)
 	{
 		fib3 = fib1 + fib2;
 		count++;
-		printf("%ld\n", fib3);
+		printf("%lu\n", fib3);
 		fib1 = fib2;
 		fib2 = fib3;
 	}
