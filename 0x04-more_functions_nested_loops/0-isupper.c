69145a4d5d7c373f3726b48f2acab85ebacb8c81
StalloneSimon <stallonendungu@gmail.com>
2022-12-31 16:14:03 +0000

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - checks for uppercase character 
+ * _isupper - checks for uppercase character
  * @c: char input
  * Return: 1 if c is uppercase, 0 otherwise
  */
