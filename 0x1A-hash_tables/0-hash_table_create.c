4631029fbeb4ddfc29b99a39c8c5d8aefb242184
StalloneSimon <Stallonendungu@gmail.com>
2024-04-14 13:16:44 +0000

    Wow

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,18 +7,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
-	unsigned long int i = 0;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (!ht)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
+
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (!ht->array)
-	{
+	if (ht->array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+
 	return (ht);
 }
